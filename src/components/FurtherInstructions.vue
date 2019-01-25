65d5710dac5e7ad5b164725ebedccca71b708610
matthewl <matthew.l@adept.co.za>
2019-01-25 12:32:07 +0200

    instructions

--- src/components/FurtherInstructions.vue
+++ src/components/FurtherInstructions.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    Test
+    <h3>We have decided that we are in need for a live weather webapp</h3>
+    <br>
+    <p>We want to be able to see <b>Cape Town</b>, <b>Johannesburg</b> and <b>Durban</b> in a list and be able to click through to them and view a 7 day forecast</p>
+    <br>
+
+    <p>We had a developers spec meeting and these are the mockups that we want you to implement:</p>
+    <ul>
+      <li>
+        Location list:
+
+      </li>
+      <li>
+        Location Forecast: 
+
+      </li>
+    </ul>
+    <p>
+      We want you to consume the <a href="https://openweathermap.org/api">Openweathermap API</a> to retrieve the live data using the following API key:<strong> c60affe7bd71ca5cf3853f5a6fba4dbb</strong>
+    </p>
+    <p><strong>Look through the documentation to find the calls you need</strong></p>
   </div>
 </template>
 
